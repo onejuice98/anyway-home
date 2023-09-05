95c4799cb4cd2576488b349e1ed901ae17eb7a97
rakimmayers <pms6413@gmail.com>
2023-09-06 01:39:35 +0900

    skeleton

---
+++ src/initial.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Title = () => {
+  return <title>
+    Anyway HOME
+  </title>
+}
+
+const Header = () => {
+  return <header>
+    <h1>Anyway HOME</h1>
+  </header>
+}
+
+const InputAddress = () => {
+  return <input type="text" placeholder="주소를 입력하세요.">
+  </input>
+}
+
+const ButtonSearchAddress = () => {
+  return <button>
+    검색
+  </button>
+}
+
+const DestinationPreview = () => {
+  return <div>
+    space for preview
+  </div>
+}
+
+const ButtonStart = () => {
+  return <button>
+    시작
+  </button>
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <Title></Title>
+      <Header></Header>
+      <InputAddress></InputAddress>
+      <ButtonSearchAddress></ButtonSearchAddress>
+      <DestinationPreview></DestinationPreview>
+      <ButtonStart></ButtonStart>
+    </div>
+  );
+}
+
+
+
+export default App;
+
+
+//
