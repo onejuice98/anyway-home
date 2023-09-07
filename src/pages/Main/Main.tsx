910f1de51305b522f29b1d305368d4d92c9a082f
onejuice98 <waterpurifier@khu.ac.kr>
2023-09-07 21:13:16 +0900

    chore : tailwindCSS 설치

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,12 @@
 import {Link} from "react-router-dom";
 
 const Main = () => {
-    return (<div> This is Main
-        <Link to="/roadview"><button >Roadview</button></Link>
-        <Link to="/credit"><button>Credit</button></Link>
+    return (<div className="m-4 mb-7"> This is Main
+        <div className="flex flex-col gap-2 mt-4">
+            <Link to="/roadview"><button className="p-4 bg-emerald-500 rounded hover:bg-emerald-700">Roadview</button></Link>
+            <Link to="/credit"><button className="p-4 bg-pink-500 rounded hover:bg-pink-700">Credit</button></Link>
+        </div>
+
     </div>)
 }
 
