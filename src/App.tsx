faeea6c61cc9dc806206f07f4af79fa676dca40e
onejuice98 <waterpurifier@khu.ac.kr>
2023-09-07 20:17:30 +0900

    fix : anyway-home directory remove * Main Componenets 생성

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Main from "./main";
+
+const Header = () => {
+  return <header>
+    <h1>Anyway HOME</h1>
+  </header>
+}
+
+const InputAddress = () => {
+  return <input type="text" placeholder="주소를 입력하세요.">
+  </input>
+}
+
+const ButtonSearchAddress = () => {
+  return <button>
+    검색
+  </button>
+}
+
+const DestinationPreview = () => {
+  return <div>
+    space for preview
+  </div>
+}
+
+const ButtonStart = () => {
+  return <button>
+    시작
+  </button>
+}
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header></Header>
+            <InputAddress></InputAddress>
+            <ButtonSearchAddress></ButtonSearchAddress>
+            <DestinationPreview></DestinationPreview>
+            <ButtonStart></ButtonStart>
+            <Main/>
+        </div>
+    );
+}
+
+
+
+export default App;
+
+
+//
