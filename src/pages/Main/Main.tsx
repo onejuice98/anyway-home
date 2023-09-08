96bcb66820aaaf4265fb29f3b9d2bb2f5597d05f
rakimmayers <pms6413@gmail.com>
2023-09-08 22:01:49 +0900

    fix : Main page 내 Credits 버튼 경로 수정 * Credits 버튼 경로 수정 Credit --> Credits

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -35,7 +35,7 @@ const Main = () => {
     return (<div className="m-4 mb-7"> This is Main
         <div className="flex flex-col gap-2 mt-4">
             <Link to="/roadview"><button className="p-4 bg-emerald-500 rounded hover:bg-emerald-700">Roadview</button></Link>
-            <Link to="/credit"><button className="p-4 bg-pink-500 rounded hover:bg-pink-700">Credit</button></Link>
+            <Link to="/credits"><button className="p-4 bg-pink-500 rounded hover:bg-pink-700">Credits</button></Link>
         </div>
 
         <div className="grid place-items-center">
