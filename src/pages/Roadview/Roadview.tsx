1324b2331a9fce390f0e845e03668a07959a166e
rakimmayers <pms6413@gmail.com>
2023-09-11 23:45:38 +0900

    fix :  Roadview 페이지 레이아웃 수정정 * Roadview 페이지 AddressToGo, Playtime, ButtonToggleMap 컴포넌트 레이아웃 변경

--- src/pages/Roadview/Roadview.tsx
+++ src/pages/Roadview/Roadview.tsx
@@ -1,52 +1,67 @@
-const AddressToGo = () => {
-    return (
-        <div className= "bg-blue-400 w-fit h-fit p-1 rounded-md text-2xl">
-            입력받은 주소
-        </div>
-    );
+import { useState } from 'react';
+
+interface ButtonToggleMapProps {
+	toggleMap: boolean;
+	onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
+
+const AddressToGo = () => {
+	return <div className="mr-3 bg-blue-400 w-fit h-fit p-1 rounded-md text-2xl">입력받은 주소</div>;
+};
 
 const Playtime = () => {
-    return (
-        <div className="bg-blue-500 w-fit h-fit p-1 rounded-md text-2xl">
-                00:00:00
-        </div>
-    );
-}
+	return <div className="bg-blue-500 w-fit h-fit p-1 rounded-md text-2xl">00:00:00</div>;
+};
 
-const ButtonToggleMap = () => {
-    return (
-        <button className="bg-orange-300 hover:bg-orange-400 w-fit h-fit p-1 rounded-md text-2xl">
-            map
-        </button>
-    );
-}
+const ButtonToggleMap = ({ toggleMap, onClick }: ButtonToggleMapProps) => {
+	return (
+		<button onClick={onClick} className="bg-orange-300 hover:bg-orange-400 w-fit h-fit p-1 rounded-md text-2xl">
+			map
+		</button>
+	);
+};
 
 const RoadviewWindow = () => {
-    return (
-        <div className="bg-blue-300 h-screen m-10 flex justify-center items-center rounded-lg">
-            roadview window
-        </div>
-    );
-}
+	return <div className="bg-blue-300 h-screen mt-3 m-5 flex justify-center items-center rounded-lg">roadview window
+    </div>;
+};
 
+const MapWindow = () => {
+	return <div className="absolute right-10 top-10 w-1/4 h-1/4 rounded-lg flex justify-center items-center bg-orange-500 w-100">map window</div>;
+};
 
 const Roadview = () => {
-    return (
-    <div>This is Roadview
+	const [toggleMap, setToggleMap] = useState(false);
 
-        <div className="flex m-3">
-            <AddressToGo />
-            <Playtime />
-            <ButtonToggleMap />
-        </div>
+	const onClick = () => {
+		if (toggleMap == false) {
+			setToggleMap(true);
+		} else {
+			setToggleMap(false);
+		}
 
-        <div>
-            <RoadviewWindow />
-        </div>
+		console.log(toggleMap);
+	};
 
-    </div>
-    );
-}
+	return (
+		<div>
+			This is Roadview
+			<div className="">
+				<AddressToGo />
+			</div>
+            <div className="flex">
+                <Playtime />
+				<ButtonToggleMap toggleMap={toggleMap} onClick={onClick} />
+            </div>
+            <div>
+				<RoadviewWindow />
+			</div>
+			{toggleMap !== false && <MapWindow />}
+		</div>
+	);
+};
 
 export default Roadview;
+
+
+
